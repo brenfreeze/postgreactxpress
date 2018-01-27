444a5df4ff009f2e7ed9dd17eed5ed25ab3d61d1
brendudu <bren24.com@gmail.com>
2018-01-26 19:38:40 -0800

    Added /PUT function

--- routes/web.js
+++ routes/web.js
@@ -12,12 +12,22 @@ const pool = new Pool({
 
 pool.connect();
 
+const client = new Client({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'postgres',
+	password: 'pass',
+	port: 5432,
+});
+
+client.connect();
+
 router.get('/', (req, res) => {
 	res.render("index", {title: "Express x React x pgSQL"});
 });
 
 router.get('/users', (req, res) => {
-	pool.query('SELECT * FROM users')
+	pool.query('SELECT * FROM users ORDER BY user_id ASC')
 		.then(result => res.send(result.rows))
 		.catch(e => res.send(e))
 });
@@ -31,6 +41,19 @@ router.post('/users', (req, res)=>{
 			res.send(result.rows[0])
 		})
 		.catch(e => res.send(e))
+});
+
+router.put('/users', (req, res) => {
+	const updatedUser = Object.values(req.body);
+	const sql = "UPDATE users SET user_name = $2, user_age = $3 WHERE user_id = $1";
+
+	pool.query(sql, updatedUser)
+		.then(result => {
+			client.query('SELECT * FROM users ORDER BY user_id ASC')
+				.then(rslt => {
+					res.send(rslt.rows);
+				})
+		});
 });
 
 module.exports = router;
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TextEditable} from './Editables';
+import axios from 'axios';
 
 export class UserProfile extends React.Component{
     constructor(props){
@@ -14,33 +15,37 @@ export class UserProfile extends React.Component{
     }
 
     changeToInput(){
-        if(this.state.toggler % 2 === 0){
-            this.setState({
-                toInput: "true"
-            });
-        } else {
-            this.setState({
-                toInput: "false"
-            });
-        }
-
         this.setState(prev => {
-            return { toggler: prev.toggler + 1 }
+            return {
+                toInput: this.state.toggler % 2 === 0 ? "true" : "false",
+                toggler: prev.toggler + 1
+            }
         });
+
+        if(this.state.toInput === "true"){
+            axios.put('/users', this.props.user)
+                .then(res => {
+                    this.props.users();
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
     }
 
-	render() {
-		return (
-			<div className="user-profile">
+    render() {
+        return (
+            <div className="user-profile">
                 <div className="user-img">
                 </div>
                 <TextEditable toInput={this.state.toInput} controlName="user_name" edClass="h1-editable" textType="heading" hClass="user-name" value={this.props.user.user_name} handleChange={this.props.userEdit} />
-                <TextEditable toInput={this.state.toInput} controlName="user_age" edClass="p-editable" textType="text" pClass="user-age" value={(this.props.user.user_age) + " years old"} handleChange={this.props.userEdit} />
+                <span className="inline"><TextEditable toInput={this.state.toInput} controlName="user_age" edClass="p-editable" textType="text" pClass="user-age" value={this.props.user.user_age} handleChange={this.props.userEdit} /><p className="user-age ml"> years old</p></span>
+
                 <div className="user-actions-container">
-                    <button className="user-action" onClick={this.changeToInput}>Edit</button>
+                    <button className="user-action" onClick={this.changeToInput}>{this.state.toInput === "true" ? "Save" : "Edit"}</button>
                     <button className="user-action">Delete</button>
                 </div>
             </div>
-		)
-	}
+        )
+    }
 }
