8b86b53f8c215e8511a9ec4f97b93420a46830d8
brenfreeze <bren24.com@gmail.com>
2018-01-27 10:38:28 +0800

    fix

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,6 +4,29 @@ import {TextEditable} from './Editables';
 export class UserProfile extends React.Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            toInput: "false",
+            ct: 0
+        }
+
+        this.makeEditable = this.makeEditable.bind(this);
+    }
+
+    makeEditable(){
+        if(this.state.ct % 2 === 0){
+            this.setState({
+                toInput: "true"
+            })   
+        } else {
+            this.setState({
+                toInput: "false"
+            })
+        }
+
+        this.setState(prev => {
+            return {ct: prev.ct + 1}
+        })
     }
 
 	render() {
@@ -11,10 +34,10 @@ export class UserProfile extends React.Component{
 			<div className="user-profile">
                 <div className="user-img">
                 </div>
-                <TextEditable toInput="true" controlName="user_name" edClass="h1-editable" textType="heading" hClass="user-name" value={this.props.user.user_name} handleChange={this.props.userEdit} />
+                <TextEditable toInput={this.state.toInput} controlName="user_name" edClass="h1-editable" textType="heading" hClass="user-name" value={this.props.user.user_name} handleChange={this.props.userEdit} />
                 <TextEditable toInput="false" controlName="user_age" textType="text" pClass="user-age" value={(this.props.user.user_age) + " years old"} />
                 <div className="user-actions-container">
-                    <button className="user-action">Edit</button>
+                    <button className="user-action" onClick={this.makeEditable}>Edit</button>
                     <button className="user-action">Delete</button>
                 </div>
             </div>
