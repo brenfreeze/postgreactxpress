71d9942502cf1f4244f1ba8b47fa17899d15fdb0
brenfreeze <bren24.com@gmail.com>
2018-01-28 10:35:38 +0800

    change port to process.env.PORT

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.set('view engine', 'pug');
 
 app.use(require('./routes/web'));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
 	console.log('Listening on port 3000');
 });
